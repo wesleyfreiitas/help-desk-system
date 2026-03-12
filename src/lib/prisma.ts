0e4cd6c13321044a483d7fad97f5f576ff4503f5
Wesley Freitas <wesley8bu@gmail.com>
2026-03-12 11:25:21 -0300

    Force DATABASE_URL in Prisma initialization to fix Vercel deploy

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,14 @@ import { PrismaClient } from '../../prisma/generated-client';
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-// Forçamos a criação de uma nova instância para pegar o novo schema
-export const prisma = new PrismaClient();
+// Forçamos a criação de uma nova instância e passamos a URL explicitamente
+// Isso resolve o erro de "Environment variable not found" na Vercel
+export const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL
+    }
+  }
+});
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
